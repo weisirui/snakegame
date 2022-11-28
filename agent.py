75c7082c1468b7af85b454741dbad2203a6616f5
Ma <jianq@northeastern.edu>
2022-11-28 00:52:35 -0800

    Implemented model and agent to apply Deep Q learning on AI and plot it

--- agent.py
+++ agent.py
@@ -3,6 +3,8 @@ import random
 import numpy as np
 from game import SnakeGameAI, Direction, Point
 from collections import deque
+from model import Linear_QNet, QTrainer
+from helper import plot
 
 MAX_MEMORY = 100_000
 BATCH_SIZE = 1000
@@ -13,10 +15,10 @@ class Agent:
     def __init__(self):
         self.n_games = 0
         self.epsilon = 0 # randomness
-        self.gamma = 0 # discount rate in the equation
+        self.gamma = 0.9 # discount rate in the equation
         self.memory = deque(maxlen=MAX_MEMORY) # popleft() when full
-        self.model = None
-        self.trainer = None
+        self.model = Linear_QNet(11,256,3)
+        self.trainer = QTrainer(self.model, lr=LR, gamma=self.gamma)
 
     def get_state(self, game):
         head = game.snake[0]
@@ -82,7 +84,7 @@ class Agent:
             final_move[move] = 1
         else:
             state0 = torch.tensor(state, dtype=torch.float)
-            prediction = self.model.predict(state0)
+            prediction = self.model(state0)
             move = torch.argmax(prediction).item()
             final_move[move] = 1
         return final_move
@@ -118,9 +120,15 @@ def train():
 
             if score > record:
                 record = score
-                # agent.model.save()
+                agent.model.save()
             
             print('Game', agent.n_games, 'Score', score, 'Record:', record)
+
+            plot_scores.append(score)
+            total_score += score
+            mean_score = total_score / agent.n_games
+            plot_mean_scores.append(mean_score)
+            plot(plot_scores, plot_mean_scores)
 
 if __name__ == '__main__':
     train()
